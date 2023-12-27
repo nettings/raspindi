ba4ec5374c78e0521e5db847d9600dfd06315e9a
Jörn Nettingsmeier <nettings@luchtbeweging.nl>
2023-12-27 21:20:57 +0100

    do not clobber existing /etc/raspindi.conf

--- install.sh
+++ install.sh
@@ -17,7 +17,9 @@ fi
 cp build/src/raspindi "$BIN_DIR"
 cp build/src/libndioutput.so "$LIB_DIR"
 
-cp etc/raspindi.conf.default /etc/raspindi.conf
+if [ ! -e /etc/raspindi.conf ]; then
+  cp etc/raspindi.conf.default /etc/raspindi.conf
+fi
 
 echo '#!/usr/bin/env sh
 LD_LIBRARY_PATH="/opt/raspindi/lib" /opt/raspindi/bin/raspindi
